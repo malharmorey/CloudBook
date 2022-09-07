b3a5f7401261e2d638a263b735fc5e7fbd52b211
Malhar <malharmorey1436@gmail.com>
2022-09-07 17:08:43 +0530

    :sparkles: Can update an existing note

--- server/routes/notes.js
+++ server/routes/notes.js
@@ -22,7 +22,7 @@ router.post(
 	'/addnote',
 	fetchuser,
 	[
-		//Validating the input from user
+		// Validating the input from user
 		body('title')
 			.isLength({
 				min: 3,
@@ -31,6 +31,7 @@ router.post(
 		body('description')
 			.exists({ checkFalsy: true })
 			.withMessage('Description can not be blank'),
+		// TODO: eliminate null string
 	],
 	async (req, res) => {
 		try {
@@ -42,7 +43,7 @@ router.post(
 				return res.status(400).json({ errors: errors.array() });
 			}
 
-			//Creating a new note
+			// Creating a new note
 			const note = new Note({
 				title,
 				description,
@@ -58,4 +59,54 @@ router.post(
 	}
 );
 
+//---------------------------------ROUTE 3---------------------------------
+// Updating an existing note of a user : get "api/notes/updatenote".Login required
+router.put(
+	'/updatenote/:id',
+	fetchuser,
+	[
+		// Validating the input from user
+		body('title')
+			.isLength({
+				min: 3,
+			})
+			.withMessage('Title should contain atleast 3 characters'),
+		body('description')
+			.exists({ checkFalsy: true })
+			.withMessage('Description can not be blank'),
+		// TODO: eliminate null string
+	],
+	async (req, res) => {
+		const { title, description, tag } = req.body;
+
+		// Creating a new note object
+		const newNote = {};
+		if (title) {
+			newNote.title = title;
+		}
+		if (description) {
+			newNote.description = description;
+		}
+		if (tag) {
+			newNote.tag = tag;
+		}
+
+		// Finding the note to be updated
+		let note = await Note.findById(req.params.id);
+		if (!note) {
+			return res.status(404).send('Note not found');
+		}
+		if (note.user.toString() !== req.user.id) {
+			return res.status(401).send('Unauthorized user');
+		}
+
+		// Updating note
+		note = await Note.findByIdAndUpdate(
+			req.params.id,
+			{ $set: newNote },
+			{ new: true }
+		);
+		res.json({ note });
+	}
+);
 module.exports = router;
