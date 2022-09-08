e988829d4a28f4f95ae610e9d5bdb9ceefb229a3
Malhar <malharmorey1436@gmail.com>
2022-09-08 10:30:31 +0530

    :sparkles: Can delete an existing note of a user
    untill now can perform  successful CRUD  operation in DB

--- server/routes/notes.js
+++ server/routes/notes.js
@@ -17,7 +17,7 @@ router.get('/getallnotes', fetchuser, async (req, res) => {
 });
 
 //---------------------------------ROUTE 2---------------------------------
-// Adding note of a user : get "api/notes/addnote".Login required
+// Adding note of a user : POST "api/notes/addnote".Login required
 router.post(
 	'/addnote',
 	fetchuser,
@@ -34,9 +34,8 @@ router.post(
 		// TODO: eliminate null string
 	],
 	async (req, res) => {
+		const { title, description, tag } = req.body;
 		try {
-			const { title, description, tag } = req.body;
-
 			// Returning bad request and error in case of any error
 			const errors = validationResult(req);
 			if (!errors.isEmpty()) {
@@ -60,7 +59,7 @@ router.post(
 );
 
 //---------------------------------ROUTE 3---------------------------------
-// Updating an existing note of a user : get "api/notes/updatenote".Login required
+// Updating an existing note of a user : PUT "api/notes/updatenote".Login required
 router.put(
 	'/updatenote/:id',
 	fetchuser,
@@ -78,35 +77,64 @@ router.put(
 	],
 	async (req, res) => {
 		const { title, description, tag } = req.body;
+		try {
+			// Creating a new note object
+			const newNote = {};
+			if (title) {
+				newNote.title = title;
+			}
+			if (description) {
+				newNote.description = description;
+			}
+			if (tag) {
+				newNote.tag = tag;
+			}
 
-		// Creating a new note object
-		const newNote = {};
-		if (title) {
-			newNote.title = title;
-		}
-		if (description) {
-			newNote.description = description;
-		}
-		if (tag) {
-			newNote.tag = tag;
-		}
+			// Finding the note to be updated
+			let note = await Note.findById(req.params.id);
+			if (!note) {
+				return res.status(404).send('Note not found');
+			}
+			// Allowing updation only if the user is authorized
+			if (note.user.toString() !== req.user.id) {
+				return res.status(401).send('Unauthorized user');
+			}
 
-		// Finding the note to be updated
+			// Updating note
+			note = await Note.findByIdAndUpdate(
+				req.params.id,
+				{ $set: newNote },
+				{ new: true }
+			);
+			res.json({ note });
+		} catch (error) {
+			console.error(error.message);
+			res.status(500).send('Internal server error');
+		}
+	}
+);
+
+//---------------------------------ROUTE 4---------------------------------
+// Deleting an existing note of a user : DELETE "api/notes/deletenote".Login required
+router.delete('/deletenote/:id', fetchuser, async (req, res) => {
+	try {
+		// Finding the note to be deleted
 		let note = await Note.findById(req.params.id);
 		if (!note) {
 			return res.status(404).send('Note not found');
 		}
+
+		// Allowing deletion only if the user is authorized
 		if (note.user.toString() !== req.user.id) {
 			return res.status(401).send('Unauthorized user');
 		}
 
 		// Updating note
-		note = await Note.findByIdAndUpdate(
-			req.params.id,
-			{ $set: newNote },
-			{ new: true }
-		);
-		res.json({ note });
+		note = await Note.findByIdAndDelete(req.params.id);
+		res.json({ Success: 'Note has been deleted', note: note });
+	} catch (error) {
+		console.error(error.message);
+		res.status(500).send('Internal server error');
 	}
-);
+});
 module.exports = router;
