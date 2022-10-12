0f57bfc710ab3f562b88b08623a29f7284c80500
Malhar <malharmorey1436@gmail.com>
2022-10-12 14:42:31 +0530

    :zap: optimized fetch API requests

--- client/src/context/notes/NoteState.js
+++ client/src/context/notes/NoteState.js
@@ -11,113 +11,127 @@ const NoteState = (props) => {
 	// GET All notes
 	const getAllNotes = async () => {
 		// API call to server
-		const response = await fetch(`${host}/api/notes/getallnotes`, {
+		await fetch(`${host}/api/notes/getallnotes`, {
 			method: 'GET',
 			headers: {
 				'Content-Type': 'application/json',
 				'auth-token': localStorage.getItem('token'),
 			},
-		});
-		const fetchedNotes = await response.json();
-		setNotes(fetchedNotes.notes);
-		setUserName(fetchedNotes.userName);
-		if (fetchedNotes.success !== true) {
-			showAlert(`${fetchedNotes.message}`, 'danger');
-		}
+		})
+			.then(async (response) => {
+				const fetchedNotes = await response.json();
+				setNotes(fetchedNotes.notes);
+				setUserName(fetchedNotes.userName);
+				// Alerts
+				if (fetchedNotes.success !== true) {
+					showAlert(`${fetchedNotes.message}`, 'danger');
+				}
+			})
+			.catch((error) => {
+				showAlert(`${error.message}`, 'danger');
+			});
 	};
 
 	// ADD note
 	const addNote = async (title, description, tag) => {
 		// API call to server
-		const response = await fetch(`${host}/api/notes/addnote`, {
+		await fetch(`${host}/api/notes/addnote`, {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json',
 				'auth-token': localStorage.getItem('token'),
 			},
 			body: JSON.stringify({ title, description, tag }),
-		});
-
-		// Client-side logic to add a note
-		const addedNote = await response.json();
-		setNotes(notes.concat(addedNote.note));
-		// Alerts
-		if (addedNote.success) {
-			showAlert(`${addedNote.message}`, 'success');
-		} else {
-			addedNote.message === undefined
-				? showAlert(`Oops unable to add your note`, 'warning')
-				: showAlert(`${addedNote.message}`, 'danger');
-		}
+		})
+			.then(async (response) => {
+				const addedNote = await response.json();
+				setNotes(notes.concat(addedNote.note));
+				// Alerts
+				if (addedNote.success) {
+					showAlert(`${addedNote.message}`, 'success');
+				} else {
+					addedNote.message === undefined
+						? showAlert(`Oops unable to add your note`, 'warning')
+						: showAlert(`${addedNote.message}`, 'danger');
+				}
+			})
+			.catch((error) => {
+				showAlert(`${error.message}`, 'danger');
+			});
 	};
 
 	// EDIT note
 	const editNote = async (id, title, description, tag) => {
 		// API call to server
-		const response = await fetch(`${host}/api/notes/updatenote/${id}`, {
+		await fetch(`${host}/api/notes/updatenote/${id}`, {
 			method: 'PUT',
 			headers: {
 				'Content-Type': 'application/json',
 				'auth-token': localStorage.getItem('token'),
 			},
 			body: JSON.stringify({ title, description, tag }),
-		});
-		// eslint-disable-next-line
-		const serverResponse = await response.json();
-		// Alerts
-		if (serverResponse.success) {
-			showAlert(`${serverResponse.message}`, 'success');
-		} else {
-			serverResponse.message === undefined
-				? showAlert(`Oops unable to update your note`, 'warning')
-				: showAlert(`${serverResponse.message}`, 'danger');
-		}
-
-		// Client-side logic to edit a note
-		let newNotes = JSON.parse(JSON.stringify(notes));
-		for (let index = 0; index < newNotes.length; index++) {
-			const element = newNotes[index];
-			if (element._id === id) {
-				newNotes[index].title = title;
-				newNotes[index].description = description;
-				newNotes[index].tag = tag;
-				newNotes[index].date = serverResponse.date;
-				break;
-			}
-		}
-		setNotes(newNotes);
+		})
+			.then(async (response) => {
+				const serverResponse = await response.json();
+				// Alerts
+				if (serverResponse.success) {
+					showAlert(`${serverResponse.message}`, 'success');
+					// Client-side logic to edit a note
+					let newNotes = JSON.parse(JSON.stringify(notes));
+					for (let index = 0; index < newNotes.length; index++) {
+						const element = newNotes[index];
+						if (element._id === id) {
+							newNotes[index].title = title;
+							newNotes[index].description = description;
+							newNotes[index].tag = tag;
+							newNotes[index].date = serverResponse.date;
+							break;
+						}
+					}
+					setNotes(newNotes);
+				} else {
+					serverResponse.message === undefined
+						? showAlert(`Oops unable to update your note`, 'warning')
+						: showAlert(`${serverResponse.message}`, 'danger');
+				}
+			})
+			.catch((error) => {
+				showAlert(`${error.message}`, 'danger');
+			});
 	};
 
 	// DELETE note
 	const deleteNote = async (id) => {
 		// API call to server
-		const response = await fetch(`${host}/api/notes/deletenote/${id}`, {
+		await fetch(`${host}/api/notes/deletenote/${id}`, {
 			method: 'DELETE',
 			headers: {
 				'Content-Type': 'application/json',
 				'auth-token': localStorage.getItem('token'),
 			},
-		});
-
-		// eslint-disable-next-line
-		const serverResponse = await response.json();
-		// Alerts
-		if (serverResponse.success) {
-			showAlert(`${serverResponse.message}`, 'danger');
-		} else {
-			serverResponse.message === undefined
-				? showAlert(`Oops unable to delete your note`, 'warning')
-				: showAlert(`${serverResponse.message}`, 'danger');
-		}
-
-		// Client-side logic to delete a note
-		let newNotesArray = notes.filter((note) => {
-			return note._id !== id;
-		});
-		setNotes(newNotesArray);
+		})
+			.then(async (response) => {
+				const serverResponse = await response.json();
+				// Alerts
+				if (serverResponse.success) {
+					showAlert(`${serverResponse.message}`, 'danger');
+					// Client-side logic to delete a note
+					let newNotesArray = notes.filter((note) => {
+						return note._id !== id;
+					});
+					setNotes(newNotesArray);
+				} else {
+					serverResponse.message === undefined
+						? showAlert(`Oops unable to delete your note`, 'warning')
+						: showAlert(`${serverResponse.message}`, 'danger');
+				}
+			})
+			.catch((error) => {
+				showAlert(`${error.message}`, 'danger');
+			});
 	};
 
-	// Clearing user notes on client side
+	// Clearing currentUser's notes on client side after logout
 	const clearUserNotesArray = () => {
 		setNotes([]);
 	};
